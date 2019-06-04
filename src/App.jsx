4740ac23e86fca8bd8b0e6b60b1112a1519989b9
David Eastmond <davebuck1981@gmail.com>
2019-06-04 21:20:08 +0000

    started implementing user input

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,18 @@ class App extends Component {
                     currentUser: 'Bob'}
   }
 
+  updateChatMessage = () => {
+    console.log("App update chat message");
+  }
   componentDidMount () {
     setTimeout(() => {
       console.log("Simulating incoming message");
       // Add a new message to the list of messages in the data store
       const newMessage = {id: "3", username: "Michelle", content: "Hello there!"};
       const messages = this.state.messagelist.concat(newMessage);
-     console.log("line 24", messages);
       // Update the state of the app component.
       // Calling setState will trigger a call to render() in App and all child components.
       this.setState({ messagelist: messages });
-      console.log(this);
     }, 3000);
   }
 
@@ -45,7 +46,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a> 
         </nav>
           <MessageList messagelist= {this.state.messagelist}/>
-        <Chatbar currentUser={this.state.currentUser}/>
+        <Chatbar currentUser={this.state.currentUser} funcUpdateChatMessage={this.updateChatMessage}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,13 +3,26 @@ import React, {Component} from 'react';
 class Chatbar extends Component {
   constructor (props) {
     super (props)
-
     this.state = {currentUser: 'Annonymous'}
+  }
+  
+  handleChatMessageChange = (e) => {
+    // Handles when user types presses enter. 
+    // Create a chat message object and sent it up to the parent
+    if (e.key === 'Enter') {
+      console.log()
+      this.props.funcUpdateChatMessage();
+    }
+  }
+  updateSender = (e) => {
+    // Update the state currentUser state property when a user types a name into the username box
+    this.setState({currentUser: e.target.value.trim()})
+    
   }
   render () {
     return (<footer className="chatbar">
-      <input className="chatbar-username" placeholder={"Your Name (Optional)"} defaultValue= {this.props.currentUser} />
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      <input className="chatbar-username" onChange={this.updateSender} placeholder={"Your Name (Optional)"} defaultValue= {this.props.currentUser} />
+      <input className="chatbar-message" onKeyPress={this.handleChatMessageChange} placeholder="Type a message and hit ENTER" />
     </footer>);
   }
 }
