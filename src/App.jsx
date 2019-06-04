1161b39c10ee4a7b4f9a288deaffef15bd452d04
David Eastmond <davebuck1981@gmail.com>
2019-06-04 15:54:13 +0000

    building header nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
-import Chatbar from './ChatBar'
+import Chatbar from './Chatbar.jsx';
+
 class App extends Component {
   render() {
-    return (
-      <Chatbar className="chatbar" />
+    return (<div>
+        <nav className="navbar"><a href="/" className="navbar-brand">Chatty</a> </nav>
+        <Chatbar/>
+      </div>
     );
   }
 }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,10 +2,10 @@ import React, {Component} from 'react';
 
 class Chatbar extends Component {
   render () {
-    return <footer>
+    return (<footer className="chatbar">
       <input className="chatbar-username" placeholder="Your Name (Optional)" />
-    <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-    </footer>
+      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+    </footer>);
   }
 }
-export default Chatbar
+export default Chatbar;
