cf940e621aa36c1de3434dd8486e13dfaf66d633
David Eastmond <davebuck1981@gmail.com>
2019-06-04 20:20:39 +0000

    fixed bug where I was referencing an old messagelist state

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,21 @@ class App extends Component {
     this.state = { messagelist: msgDB.messages,
                     currentUser: 'Bob'}
   }
+
+  componentDidMount () {
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: "3", username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messagelist.concat(newMessage);
+     console.log("line 24", messages);
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({ messagelist: messages });
+      console.log(this);
+    }, 3000);
+  }
+
   render() {
     return (<div>
         <nav className="navbar">
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,17 +3,10 @@ import Message from './Message.jsx';
 
 // The list container for messages.
 class MessageList extends Component {
-  constructor(props) {
-    // MessageList is constructed with the messages from the parent container (the app)
-    super (props)
-    const listOfMessages = this.props.messagelist;
-    this.state = { messageArray: listOfMessages };
-  }
-
   render () {
     // Iterate through the list of Messages and populate the MessageList window
     return (<div>
-      {this.state.messageArray.map((element) => {
+      {this.props.messagelist.map((element) => {
         return <Message key={element.id} username= {element.username} content= {element.content}></Message>
       })}
     </div>);
