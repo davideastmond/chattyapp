c2b05c380a8fe1ffe2aa69bed021796a5cf23f0b
David Eastmond <davebuck1981@gmail.com>
2019-06-06 16:17:51 +0000

    added a user counter component

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,17 +3,6 @@ import React, {Component} from 'react';
 // This holds an actual message, which contains the username and the message
 // body
 class Message extends Component {
-  /*
-  constructor(props) {
-    super (props);
-
-    this.state = {
-      type: "incomingMessage",
-      username: this.props.username,
-      messagebody: this.props.content
-    }
-  }  */
-
   render () {
     return <div className="message">
       <span className="message-username">{this.props.username}</span>
---
+++ src/chatCounter.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+/**
+ * This is the component that will be rendered to the nav bar, which keeps
+ * track of the user count
+ */
+class ChatCounter extends React {
+
+}
