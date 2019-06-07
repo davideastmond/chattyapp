e8bfe423b9b818c4f436d4668855537ae3818ed4
David Eastmond <davebuck1981@gmail.com>
2019-06-07 23:47:49 +0000

    removed debug console.log in imagepicture.jsx

--- src/imagepicture.jsx
+++ src/imagepicture.jsx
@@ -4,7 +4,6 @@ import React, {Component} from 'react';
 // body. It also supports one image included in the text
 class MessageWithImage extends Component {
   render () {
-    console.log("Message RENDERED line 7 PROPS", this.props.imageSrc);
     return <div className="message">
       <span className="message-username" style={{color: this.props.textColor }}>{this.props.username}</span>
       <span className="message-content">{this.props.content} <br></br>
